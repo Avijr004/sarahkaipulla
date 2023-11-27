f44799033943b0cf1e8bbe4dab20513d6d8212d0
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 16:10:22 +0530

    Update stats.py

--- ShizukaXMusic/plugins/tools/stats.py
+++ ShizukaXMusic/plugins/tools/stats.py
@@ -44,7 +44,7 @@ STATS_COMMAND = get_command("STATS_COMMAND")
 
 
 @app.on_message(
-    filters.command(STATS_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(STATS_COMMAND) & filters.group & ~BANNED_USERS
 )
 @language
 async def stats_global(client, message: Message, _):
