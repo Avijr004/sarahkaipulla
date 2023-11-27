910ca71787498396ed9361a90d3d708fe73661ce
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 14:10:51 +0530

    Update start.py

--- ShizukaXMusic/plugins/bot/start.py
+++ ShizukaXMusic/plugins/bot/start.py
@@ -1,6 +1,7 @@
 import asyncio
 
 from pyrogram import filters
+from pyrogram.enums import ChatType
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
