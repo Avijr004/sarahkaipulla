fe604cde496f1f046ee1ae5c198bb8cdea11eb48
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 14:10:11 +0530

    Update start.py

--- ShizukaXMusic/plugins/bot/start.py
+++ ShizukaXMusic/plugins/bot/start.py
@@ -237,7 +237,7 @@ async def welcome(client, message: Message):
             _ = get_string(language)
             if member.id == app.id:
                 chat_type = message.chat.type
-                if chat_type != "supergroup":
+                if chat_type != "ChatType.SUPERGROUP":
                     await message.reply_text(_["start_6"])
                     return await app.leave_chat(message.chat.id)
                 if chat_id in await blacklisted_chats():
