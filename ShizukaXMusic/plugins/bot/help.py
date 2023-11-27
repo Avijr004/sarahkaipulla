5b8e6a9ccafd8157c3a4533ca28f08ce16f23a5f
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 14:58:09 +0530

    Update help.py

--- ShizukaXMusic/plugins/bot/help.py
+++ ShizukaXMusic/plugins/bot/help.py
@@ -17,7 +17,7 @@ HELP_COMMAND = get_command("HELP_COMMAND")
 
 
 @app.on_message(
-    filters.command(HELP_COMMAND) & filters.private & ~filters.edited & ~BANNED_USERS
+    filters.command(HELP_COMMAND) & filters.private & ~BANNED_USERS
 )
 @app.on_callback_query(filters.regex("settings_back_helper") & ~BANNED_USERS)
 async def helper_private(
