2b2149bfeb8794f3f8e5238187086e766f0380f2
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 14:22:14 +0530

    Update formatters.py

--- ShizukaXMusic/utils/formatters.py
+++ ShizukaXMusic/utils/formatters.py
@@ -84,6 +84,33 @@ def seconds_to_min(seconds):
     return "-"
 
 
+def check_duration(file_path):
+    command = [
+        "ffprobe",
+        "-loglevel",
+        "quiet",
+        "-print_format",
+        "json",
+        "-show_format",
+        "-show_streams",
+        file_path,
+    ]
+
+    pipe = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    out, err = pipe.communicate()
+    _json = json.loads(out)
+
+    if "format" in _json:
+        if "duration" in _json["format"]:
+            return float(_json["format"]["duration"])
+
+    if "streams" in _json:
+        for s in _json["streams"]:
+            if "duration" in s:
+                return float(s["duration"])
+
+    return "Unknown"
+
 formats = [
     "webm",
     "mkv",
