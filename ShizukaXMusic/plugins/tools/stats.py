5720506d1915634ac704c87f6ad1654af16e523c
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-27 16:09:42 +0530

    Update stats.py

--- ShizukaXMusic/plugins/tools/stats.py
+++ ShizukaXMusic/plugins/tools/stats.py
@@ -57,7 +57,7 @@ async def stats_global(client, message: Message, _):
 
 
 @app.on_message(
-    filters.command(GSTATS_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
+    filters.command(GSTATS_COMMAND) & filters.group & ~BANNED_USERS
 )
 @language
 async def gstats_global(client, message: Message, _):
