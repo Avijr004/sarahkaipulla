9600258460c4a93c0c676bf63d6158f29c768e5d
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-12-02 17:52:48 +0530

    Update couples.py

--- ShizukaXMusic/plugins/modules/couples.py
+++ ShizukaXMusic/plugins/modules/couples.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from pyrogram import filters
 from pyrogram.enums import ChatType
 
-from ShizukaXMusic import app
+from ShizukaXMusic.utils.database import get_couple, save_couple
 
 
 # Date and time
