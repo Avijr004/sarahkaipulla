0684fdb5437081dd025a13aeb37ec77b94207c1b
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-12-03 10:20:24 +0530

    Update couples.py

--- ShizukaXMusic/plugins/modules/couples.py
+++ ShizukaXMusic/plugins/modules/couples.py
@@ -82,7 +82,7 @@ __New couple of the day can be chosen at 12AM {tomorrow}__"""
 
 {b1_id} + {c1_name} + {c2_name} = 😘
 __New couple of the day can be chosen at 12AM {tomorrow}__"""
-            await app.send_message(message.chat.id, text=couple_selection_message, photo=love)
+            await app.send_message(message.chat.id, text=couple_selection_message)
     except Exception as e:
         print(e)
         await message.reply_text(e)
